6a0372ebb42b310d5344cc4751085e84ef134cbd
OronAvisrur <oronavisrur@gmail.com>
2025-12-16 12:23:06 +0200

    feat: add authentication service
    - Add register function calling POST /auth/register
    - Add login function calling POST /auth/login
    - Store JWT token and username in localStorage
    - Add logout function to clear stored credentials
    - Add utility functions: getToken, getUser, isAuthenticated
    - Add setAuthHeader to configure axios Authorization header

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,50 @@
+import api from './api';
+
+const TOKEN_KEY = 'atlas_token';
+const USER_KEY = 'atlas_user';
+
+export const register = async (username, password) => {
+  const response = await api.post('/auth/register', {
+    username,
+    password
+  });
+  return response.data;
+};
+
+export const login = async (username, password) => {
+  const response = await api.post('/auth/login', {
+    username,
+    password
+  });
+  
+  const { access_token } = response.data;
+  localStorage.setItem(TOKEN_KEY, access_token);
+  localStorage.setItem(USER_KEY, username);
+  
+  return response.data;
+};
+
+export const logout = () => {
+  localStorage.removeItem(TOKEN_KEY);
+  localStorage.removeItem(USER_KEY);
+};
+
+export const getToken = () => {
+  return localStorage.getItem(TOKEN_KEY);
+};
+
+export const getUser = () => {
+  return localStorage.getItem(USER_KEY);
+};
+
+export const isAuthenticated = () => {
+  return !!getToken();
+};
+
+export const setAuthHeader = (token) => {
+  if (token) {
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  } else {
+    delete api.defaults.headers.common['Authorization'];
+  }
+};
